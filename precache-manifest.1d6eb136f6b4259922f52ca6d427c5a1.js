bad3690b6fa3932ee7bb1202463469ad7fb22ed6
Buvan Prithivee <buvan.prith21@gmail.com>
2020-05-11 17:38:28 +0530

    Updates

---
+++ precache-manifest.1d6eb136f6b4259922f52ca6d427c5a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c560bd9749631f477595a76ad0658ce",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "40cf4acd00b27b8c5df8",
+    "url": "/todo-app/static/css/main.65999341.chunk.css"
+  },
+  {
+    "revision": "ab38089449e2bac7b28c",
+    "url": "/todo-app/static/js/2.30a5b71b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-app/static/js/2.30a5b71b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40cf4acd00b27b8c5df8",
+    "url": "/todo-app/static/js/main.b6c8bf81.chunk.js"
+  },
+  {
+    "revision": "672d1f4664f6508537da",
+    "url": "/todo-app/static/js/runtime-main.6751aa9a.js"
+  }
+]);
