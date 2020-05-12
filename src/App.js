50ff5f33fc9e5f2df4f9d123d31db0a303d068df
Buvan Prithivee <buvan.prith21@gmail.com>
2020-05-12 14:13:30 +0530

    Updated on 12, May

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { GoogleLogin } from 'react-google-login';
 import './App.css';
 import Header from './components/Header';
 import AddItem from './components/AddItem';
@@ -7,15 +8,37 @@ import {todoContext} from './components/TodoContext';
 
 function App() {
   const [todoList, setTodoList] = useState([]);
-
+  const [retry, setRetry] = useState(false);
+  const todoLogin = (response) =>{
+    console.log(response);
+    setRetry(false);
+  }
+  const retryLogin = (response) => {
+    console.log(response);
+    setRetry(true);
+  }
   return (
-    <todoContext.Provider value={[todoList, setTodoList]}>
       <div className="App">
-        <Header />
-        <TodoList />
-        <AddItem />
+        <div className='Home'>
+          <Header />
+          <todoContext.Provider value={[todoList, setTodoList]}>
+            <TodoList />
+            <AddItem />
+          </todoContext.Provider>
+        </div>
+        <div className='login-container'>
+          <div className='login'>
+            <p className='login-label'>Please Login to Continue!</p>
+            <GoogleLogin
+              clientId="309767756422-d3026dgiuc5f8s2oa15idgtoc8rd99qi.apps.googleusercontent.com"
+              onSuccess={todoLogin}
+              onFailure={retryLogin}
+              cookiePolicy={'single_host_origin'}
+            />
+            <p className='error-login'>{retry ? 'Please try again!' : ''}</p>
+          </div>
+        </div>
       </div>
-    </todoContext.Provider>
   );
 }
 
